3af2ad2675ed50d0bc31199a53e0d2c6b0299d13
cheesyx <apxutektop.blitz1111@gmail.com>
2026-09-23 08:12:59 +0300

    Update proxy_parser.py

--- proxy_parser.py
+++ proxy_parser.py
@@ -9,7 +9,7 @@ import time
 import sys
 import os
 
-SUBSCRIPTION_URL = "https://ssconnect.app/?url_ha=https://flaregate.dedyn.io/api/v1/sub/7wH9ySRsmQizQNdQjkcing"
+SUBSCRIPTION_URL = "https://ssconnect.app/?url_ha=https://flaregate.dedyn.io/api/v1/sub/vzrGejo9nND3VsEpqeeW6g"
 OUTPUT = Path("frgt.txt")
 CACHE_DIR = Path(".cache")
 CACHE_FILE = CACHE_DIR / "frgt.txt"
